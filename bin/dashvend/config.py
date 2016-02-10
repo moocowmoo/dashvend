263840e067ed61e9c5711a387cca6c6794cdd527
moocowmoo <moocowmoo@masternode.me>
2016-02-10 08:20:05 +0000

    doc - indicate all path-change locations

--- bin/dashvend/config.py
+++ bin/dashvend/config.py
@@ -1,5 +1,10 @@
 # set this to the full path to the dashvend respository checkout
 DASHVEND_DIR = '/home/pi/dashvend'
+# note: also update paths in:
+#   bin/.init.d.dashvend
+#   bin/_dashvend_control.sh
+#   bin/dashvend_screens.screenrc
+#   bin/show_screen_number.sh
 
 # after testing, set this to True to use mainnet
 MAINNET = False
